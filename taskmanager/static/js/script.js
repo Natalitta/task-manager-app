53fd6c0757ace96272102a312c3fe2736c6836d1
Natalitta <>
2023-07-10 14:48:32 +0000

    Add add task page and functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // sidenav Initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker Initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker,{
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select Initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+    
 });
